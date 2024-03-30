47f4a4a63cc90d1ecabeea5afefe5de6dc1e821e
Mohsin Alam <greenstore987@gmail.com>
2024-03-30 13:51:58 +0530

    utils constomization

--- src/server.js
+++ src/server.js
@@ -4,12 +4,21 @@ import dotenv from "dotenv"
 // import mongoose from 'mongoose';
 // import { DB_NAME } from './constants';
 import connectDB from './db/db.js';
+import {app} from './app.js'
+const port = process.env.PORT || 5000
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB().then(()=>{
+ 
+    app.listen(port , ()=>{
+console.log(`Server is running at port : ${port}`);
+    })
+}).catch ((err)=>{
+    console.log("mongoDB connection failed !!! ",err);
+});
 
 
 /*
